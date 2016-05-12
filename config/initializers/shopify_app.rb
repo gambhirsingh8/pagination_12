61f54135031e6a4f12c8b0771940b6060f89fba0
users <gambhirsingh8@gmail.com>
2016-05-12 15:46:37 +0530

    rails new app

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "a2894eed659563976706e8cde64aa559"
   config.secret = "f57892e74c4e7965fef962ef4523c4b9"
-  redirect_uri = https://damp-sands-73967.herokuapp.com/auth/shopify/callback
+  redirect_uri = "https://damp-sands-73967.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
